9069c58c1ce364fce0df2e713d5795126781bc55
JoseUgal <ugal.luque.jose.m@alumat.copernic.cat>
2021-05-11 13:10:49 +0200

    Menu implementation on DashboardActivity

--- app/src/main/java/net/codefastly/yumekai/activities/Dashboard/DashboardActivity.kt
+++ app/src/main/java/net/codefastly/yumekai/activities/Dashboard/DashboardActivity.kt
@@ -2,11 +2,19 @@ package net.codefastly.yumekai.activities.Dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.codefastly.yumekai.R
+import net.codefastly.yumekai.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        this.binding = ActivityDashboardBinding.inflate( layoutInflater )
+        setContentView( this.binding.root )
+
     }
+
+
 }
