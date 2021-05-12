f17e8f6fccc1fdcdbd9c505733000927fcb5a7e7
MSI-DANIEL <leon.herrero.daniel@alumnat.copernic.cat>
2021-05-12 12:27:13 +0200

    Day Of Week Function

--- app/src/main/java/net/codefastly/yumekai/fragments/Calendar/CalendarFragment.kt
+++ app/src/main/java/net/codefastly/yumekai/fragments/Calendar/CalendarFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import net.codefastly.yumekai.R
+import net.codefastly.yumekai.databinding.FragmentCalendarBinding
+import java.time.LocalDate
+import java.util.*
 
 class CalendarFragment : Fragment() {
 
+    private lateinit var binding: FragmentCalendarBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        return binding.root
     }
 
+
+    override fun onStart() {
+        binding.calenderLblDay.setText(obtainDayOfWeek())
+        super.onStart()
+    }
+
+    fun obtainDayOfWeek(): String {
+        var dayString: String = ""
+        var calendar: Calendar = Calendar.getInstance()
+        var dayInt: Int = calendar.get(Calendar.DAY_OF_WEEK)
+
+        when(dayInt){
+            Calendar.SUNDAY -> dayString = getString(R.string.Sunday)
+            Calendar.MONDAY -> dayString = getString(R.string.Monday)
+            Calendar.TUESDAY -> dayString = getString(R.string.Tuesday)
+            Calendar.WEDNESDAY -> dayString = getString(R.string.Wednesday)
+            Calendar.THURSDAY -> dayString = getString(R.string.Thursday)
+            Calendar.FRIDAY -> dayString = getString(R.string.Feiday)
+            Calendar.SATURDAY -> dayString = getString(R.string.Saturday)
+        }
+        return dayString
+    }
 }
