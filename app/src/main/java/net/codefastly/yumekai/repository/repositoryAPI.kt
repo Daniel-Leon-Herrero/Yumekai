59d3dc9886490112e85057bf2ae862a888504511
MSI-DANIEL <leon.herrero.daniel@alumnat.copernic.cat>
2021-05-13 13:36:20 +0200

    RecycleView Data Observable When Change

--- app/src/main/java/net/codefastly/yumekai/repository/repositoryAPI.kt
+++ app/src/main/java/net/codefastly/yumekai/repository/repositoryAPI.kt
@@ -2,14 +2,42 @@ package net.codefastly.yumekai.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.codefastly.yumekai.helpers.interfaces.APIService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class repositoryAPI {
-/*
-    fun getCalenderAnime(): LiveData<MutableList<String>> {
-        val mutableData = MutableLiveData<String>()
 
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://api.jikan.moe/v3/schedule/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
 
+    fun getCalenderAnime(day: String): LiveData<MutableList<String>> {
+        val mutableData = MutableLiveData<MutableList<String>>()
 
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                val call = getRetrofit().create(APIService::class.java)
+                    .getCalendarAnimes("$day")
+                val datos = call.body()
+                withContext(Dispatchers.Main) {
+                    if (call.isSuccessful) {
+                        val images: MutableList<String> = arrayListOf()
+                        datos?.day?.forEach { day ->
+                            images.add(day.image_url)
+                        }
 
-    }*/
+                        mutableData.value = images
+                    }
+                }
+
+            }
+        }
+        return mutableData
+    }
 }
