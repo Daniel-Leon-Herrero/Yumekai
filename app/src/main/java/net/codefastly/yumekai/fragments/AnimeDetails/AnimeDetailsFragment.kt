447efb56f7a21564dcce9849f59caa1edcbeb698
JoseUgal <ugal.luque.jose.m@alumat.copernic.cat>
2021-05-13 09:59:51 +0200

    Implements Anime Details Fragment ( No iclude RecyclerViews )

---
+++ app/src/main/java/net/codefastly/yumekai/fragments/AnimeDetails/AnimeDetailsFragment.kt
@@ -0,0 +1,20 @@
+package net.codefastly.yumekai.fragments.AnimeDetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import net.codefastly.yumekai.R
+
+class AnimeDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_anime_details, container, false)
+    }
+
+}
