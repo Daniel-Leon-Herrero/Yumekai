a6700b846d4125c86622d52961cf8a399feb8275
JoseUgal <ugal.luque.jose.m@alumat.copernic.cat>
2021-05-20 19:33:07 +0200

    Start new DashboardFullScreen and return to normal Dashboard

---
+++ app/src/main/java/net/codefastly/yumekai/activities/DashboardFullScreen/DashboardFullScreen.kt
@@ -0,0 +1,12 @@
+package net.codefastly.yumekai.activities.DashboardFullScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.codefastly.yumekai.R
+
+class DashboardFullScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_full_screen)
+    }
+}
